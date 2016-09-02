20337255d12fbe5a337da899db221f3e8dc08597
ijustynclark@gmail.com <jjohnson@historynet.com>
2016-09-02 14:55:08 -0700

    Add font awesome cdn link

--- header-hero.php
+++ header-hero.php
@@ -26,6 +26,7 @@
 	    <?php } ?>
 
 		<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>">
+	<link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-T8Gy5hrqNKT+hzMclPo118YTQO6cYprQmhrYwIiQ/3axmI1hQomh7Ud2hPOy8SP1" crossorigin="anonymous">
 
 		<?php wp_head(); ?>
 
