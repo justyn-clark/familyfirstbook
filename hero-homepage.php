4902d89aa1ccc1f4a7cdf64a45d1ab4bd0a0c6e0
ijustynclark@gmail.com <jjohnson@historynet.com>
2016-09-02 15:00:00 -0700

    Add content to hero homepage template

--- hero-homepage.php
+++ hero-homepage.php
@@ -13,11 +13,106 @@
 
     <main id="main" class="large-12 medium-12 columns" role="main">
 
-<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+    <div class="large-12 columns text-center">
+    <!--<h1>FAMILY FIRST: HOW TO BE A HERO AT HOME<BR> 9 STRATEGIES FOR THE SUCCESSFUL FAMILY MAN</h1>-->
+    <img src="wp-content/themes/familyfirstbook/assets/images/Family_First_Book_Cover.png" />
 
-<?php get_template_part( 'parts/loop', 'page-home' ); ?>
+    <div class="share-book">
+    <a href="http://www.facebook.com/share.php?u=http://familyfirstbook.com/&amp;t=Family First: How to Be a Hero at Home" onclick="javascript:window.open(this.href,
+'', 'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=600,width=600');return false;" class="facebook"><i class="fa fa-facebook"></i></a>
+<a href="http://twitter.com/home?status=Family First: How to Be a Hero at Home - http://familyfirstbook.com/" onclick="javascript:window.open(this.href,
+'', 'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=600,width=600');return false;" class="twitter"><i class="fa fa-twitter"></i></a>
+<a href="javascript:pinIt();" class="pinterest"><i class="fa fa-pinterest"></i></a>
+    <a href="https://plus.google.com/share?url=http://familyfirstbook.com/" onclick="javascript:window.open(this.href,
+'', 'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=600,width=600');return false;" class="google"><i class="fa fa-google-plus"></i></a>
+</div>
 
-<?php endwhile; endif; ?>
+    </div>
+
+    <div class="large-12 columns text-center">
+    <p>I have written this book for husbands and fathers of all faiths and
+    beliefs. The concepts of love, sharing, listening, and understanding
+    are presented with the hope that each reader can feel my heart as
+    I share my story of continued striving to become a hero at home.
+    My deepest wish is that you will respect my personal journey and be
+    inspired to make your own changes in your relationships with your family members.</p>
+
+<p>I am honored to share my story with you. I hope you will benefit
+greatly from what I have experienced. My encouragement for you is
+to become the very best husband and father you can be. Wishing you
+a wonderful year of exciting growth for you and your family!
+    With deepest respect for you and the journey we share.</p>
+
+    </div>
+<hr>
+<section id="about">
+<div class="large-12 columns text-center" >
+    <h2>ABOUT THE BOOK</h2>
+<img class="Family_First_Art" src="wp-content/themes/familyfirstbook/assets/images/Family_First_Art.jpg" />
+    <p>
+This is the question Curtis R. Estes asked himself eighteen months ago—and
+the honest answer was, “No.” In this powerful book, Curtis reflects on what it
+means to raise children in the way of the Lord and how husbands can and
+should support and honor their wives as the role models of warmth,
+    compassion, and love.
+</p>
+</div>
+    </section>
+<hr>
+<section id="reviews">
+<div class="reviews">
+    <h2>REVIEWS</h2>
+    <div class="media-object stack-for-small">
+    <div class="media-object-section">
+    <img class="thumbnail" src="http://placehold.it/200x200">
+    </div>
+    <div class="media-object-section">
+
+<p>“I became a father at a young age, and it took me years to find my rhythm.
+
+    I wish I had read Family First right off the bat!”
+
+<h5><strong>Metta World Peace</strong> | Los Angeles Lakers<h5></p>
+</div>
+</div>
+
+<div class="media-object stack-for-small">
+    <div class="media-object-section">
+    <img class="thumbnail" src="http://placehold.it/200x200">
+    </div>
+    <div class="media-object-section">
+    <p>“I became a father at a young age, and it took me years to find my rhythm.
+
+    I wish I had read Family First right off the bat!”
+
+<h5><strong>Metta World Peace</strong> | Los Angeles Lakers<h5></p>
+</div>
+</div>
+
+<div class="media-object stack-for-small">
+    <div class="media-object-section">
+    <img class="thumbnail" src="http://placehold.it/200x200">
+    </div>
+    <div class="media-object-section">
+    <p>“I became a father at a young age, and it took me years to find my rhythm.I wish I had read Family First right off the bat!”
+<h5><strong>Metta World Peace</strong> | Los Angeles Lakers<h5></p>
+</div>
+
+</div>
+
+</section>
+
+<div class="share-book">
+    <a href="http://www.facebook.com/share.php?u=http://familyfirstbook.com/&amp;t=Family First: How to Be a Hero at Home" onclick="javascript:window.open(this.href,
+'', 'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=600,width=600');return false;" class="facebook"><i class="fa fa-facebook"></i></a>
+<a href="http://twitter.com/home?status=Family First: How to Be a Hero at Home - http://familyfirstbook.com/" onclick="javascript:window.open(this.href,
+'', 'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=600,width=600');return false;" class="twitter"><i class="fa fa-twitter"></i></a>
+<a href="javascript:pinIt();" class="pinterest"><i class="fa fa-pinterest"></i></a>
+    <a href="https://plus.google.com/share?url=http://familyfirstbook.com/" onclick="javascript:window.open(this.href,
+'', 'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=600,width=600');return false;" class="google"><i class="fa fa-google-plus"></i></a>
+</div>
+</div>
+
 
 </main> <!-- end #main -->
 
@@ -25,5 +120,5 @@
 
 </div> <!-- end #content -->
 
-<?php /*get_footer();*/ ?>
+<?php get_footer(); ?>
 
