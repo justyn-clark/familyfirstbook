93e87a987a251662b7f11e9c46b82f265ae49aac
ijustynclark@gmail.com <jjohnson@historynet.com>
2016-09-02 17:33:34 -0700

    Add smooth scrolling

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,4 +1,19 @@
 jQuery(document).foundation();
+jQuery(document).ready(function ($) {
+    $('a[href*=#]:not([href=#])').click(function() {
+        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') || location.hostname == this.hostname) {
+
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+            if (target.length) {
+                $('html,body').animate({
+                    scrollTop: target.offset().top
+                }, 1000);
+                return false;
+            }
+        }
+    });
+});
 /*
 These functions make sure WordPress
 and Foundation play nice together.
