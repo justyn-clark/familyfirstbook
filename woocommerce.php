9f289ff70ac34d4c8e4c4c78edb4db24bd83c914
ijustynclark@gmail.com <jjohnson@historynet.com>
2016-09-21 00:30:43 -0700

    Add woocommerce.php file

---
+++ woocommerce.php
@@ -0,0 +1,17 @@
+<?php get_header(); ?>
+	
+	<div id="content">
+	
+		<div id="inner-content" class="row">
+	
+		    <main id="main" class="large-12 medium-12 columns" role="main">
+
+				<?php woocommerce_content(); ?>
+
+			</main> <!-- end #main -->
+
+		</div> <!-- end #inner-content -->
+
+	</div> <!-- end #content -->
+
+<?php get_footer(); ?>
