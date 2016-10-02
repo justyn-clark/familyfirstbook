5527c41e84adb873664c4d110a0dd4ad471258cd
ijustynclark@gmail.com <jjohnson@historynet.com>
2016-10-02 03:04:08 -0700

    Replace event .click() method with .on()

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,6 +1,6 @@
 jQuery(document).foundation();
 jQuery(document).ready(function ($) {
-    $('a[href*=#]:not([href=#])').click(function() {
+    $('a[href*=#]:not([href=#])').on('click',function() {
         if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') || location.hostname == this.hostname) {
 
             var target = $(this.hash);
